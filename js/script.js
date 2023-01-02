295898488746cb466ed90f04fc2772ab5f663c7a
chynaraDev <batyrovnach@gmail.com>
2023-01-02 21:28:19 +0600

    footer

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 4.5,
+            spaceBetween: 50,
+        },
+    },
+});
